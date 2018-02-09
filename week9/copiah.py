b841c9908613e5f92f299b2694983d8a424581df
ezefranca <ezequiel.ifsp@gmail.com>
2018-02-09 10:48:07 -0200

    ignore podfile.lock

--- week9/copiah.py
+++ week9/copiah.py
@@ -2,165 +2,46 @@
 import re
 
 def le_assinatura():
-    """
-    A funcao le os valores dos tracos linguisticos do modelo e devolve uma
-    assinatura a ser comparada com os textos fornecidos.
-    """
-    print("Bem-vindo ao detector automático de COH-PIAH.")
+  '''A funcao le os valores dos tracos linguisticos do modelo e devolve uma assinatura a ser comparada com os textos fornecidos'''
+  print("Bem-vindo ao detector automático de COH-PIAH.")
 
-    tam_m_pal = float(input("Entre o tamanho medio de palavra:"))
-    type_token = float(input("Entre a relação Type-Token:"))
-    h_lego = float(input("Entre a Razão Hapax Legomana:"))
-    tam_m_sent = float(input("Entre o tamanho médio de sentença:"))
-    compx_med = float(input("Entre a complexidade média da sentença:"))
-    tam_m_frase = float(input("Entre o tamanho medio de frase:"))
+  wal = float(input("Entre o tamanho medio de palavra:"))
+  ttr = float(input("Entre a relação Type-Token:"))
+  hlr = float(input("Entre a Razão Hapax Legomana:"))
+  sal = float(input("Entre o tamanho médio de sentença:"))
+  sac = float(input("Entre a complexidade média da sentença:"))
+  pal = float(input("Entre o tamanho medio de frase:"))
 
-    return [tam_m_pal, type_token, h_lego, tam_m_sent, compx_med, tam_m_frase]
-
-
+  return [wal, ttr, hlr, sal, sac, pal]
+  
 def le_textos():
     i = 1
     textos = []
-    texto = input("Digite o texto " + str(i) + "(aperte enter para sair):")
+    texto = input("Digite o texto " + str(i) +" (aperte enter para sair):")
     while texto:
         textos.append(texto)
         i += 1
-        texto = input("Digite o texto " + str(i) + "(aperte enter para sair):")
+        texto = input("Digite o texto " + str(i) +" (aperte enter para sair):")
+        
     return textos
 
-
-def calcula_assinatura(texto):
-    """
-    Essa funcao recebe um texto e deve devolver a assinatura
-    do texto.
-    """
-    if type(texto) != list:
-        aux = texto
-        texto = []
-        texto.append(aux)
-    for i in texto:
-        sentencas = []
-        sentencas = separa_sentencas(str(i))  # sent.. = lista comum, ~matriz
-        frases = []
-        num_tot_sentencas = 0
-        soma_cat_sentencas = 0
-        for i in range(len(sentencas)):
-            frase_i = separa_frases(str(sentencas[i]))
-            frases.append(frase_i)  # frases = matriz, lista de listas
-            num_tot_sentencas += 1
-            soma_cat_sentencas = soma_cat_sentencas + len(sentencas[i])
-        palavras = []
-        num_tot_frases = 0
-        soma_cat_frases = 0
-        for lin in range(len(frases)):
-            for col in range(len(frases[lin])):
-                palavra_i = separa_palavras(str(frases[lin][col]))
-                palavras.append(palavra_i)  # palav.. = matriz, lista de listas
-                num_tot_frases += 1
-                soma_cat_frases = soma_cat_frases + len(str(frases[lin][col]))
-        mtrx_para_lista = []  # transform.. palavras de matriz para lista
-        for lin in range(len(palavras)):
-            for col in range(len(palavras[lin])):
-                mtrx_para_lista.append(palavras[lin][col])
-        palavras = mtrx_para_lista[:]
-        soma_comp_palavras = 0
-        num_tot_palavras = 0
-        for lin in range(len(palavras)):
-            for col in range(len(palavras[lin])):
-                soma_comp_palavras = soma_comp_palavras + len(str(palavras[lin][col]))
-            num_tot_palavras += 1
-        matriz_ass_input = []
-        matriz_ass_input.append(tam_m_pal(soma_comp_palavras, num_tot_palavras))
-        matriz_ass_input.append(type_token(palavras, num_tot_palavras))
-        matriz_ass_input.append(h_lego(palavras, num_tot_palavras))
-        matriz_ass_input.append(tam_m_sent(soma_cat_sentencas, num_tot_sentencas))
-        matriz_ass_input.append(compx_med(num_tot_frases, num_tot_sentencas))
-        matriz_ass_input.append(tam_m_frase(soma_cat_frases, num_tot_frases))
-    return matriz_ass_input  # matriz, lista de listas dos valores das assina..
-
-
-def tam_m_pal(soma_comp_palavras, num_tot_palavras):
-    if num_tot_palavras != 0:
-        tam_m_pal = soma_comp_palavras / num_tot_palavras
-    else:
-        tam_m_pal = 0
-    return tam_m_pal
-
-
-def type_token(lista_palavras, num_tot_palavras):
-    num_pal_dif = n_palavras_diferentes(lista_palavras)
-    if num_tot_palavras != 0:
-        type_token = num_pal_dif / num_tot_palavras
-    else:
-        type_token = 0
-    return type_token
-
-
-def h_lego(lista_palavras, num_tot_palavras):
-    num_pal_uni = n_palavras_unicas(lista_palavras)
-    if num_tot_palavras != 0:
-        h_lego = num_pal_uni / num_tot_palavras
-    else:
-        h_lego = 0
-    return h_lego
-
-
-def tam_m_sent(soma_num_cat, num_sent):
-    if num_sent != 0:
-        tam_m_sent = soma_num_cat / num_sent
-    else:
-        tam_m_sent = 0
-    return tam_m_sent
-
-
-def compx_med(num_tot_frases, num_tot_sentencas):
-    if num_tot_sentencas != 0:
-        compx_med = num_tot_frases / num_tot_sentencas
-    else:
-        compx_med = 0
-    return compx_med
-
-
-def tam_m_frase(soma_cat_frases, num_tot_frases):
-    if num_tot_frases != 0:
-        tam_m_frase = soma_cat_frases / num_tot_frases
-    else:
-        tam_m_frase = 0
-    return tam_m_frase
-
-
 def separa_sentencas(texto):
-    """
-    A funcao recebe um texto e devolve uma lista das sentencas dentro
-    do texto.
-    """
+    '''A funcao recebe um texto e devolve uma lista das sentencas dentro do texto'''
     sentencas = re.split(r'[.!?]+', texto)
     if sentencas[-1] == '':
         del sentencas[-1]
     return sentencas
 
-
 def separa_frases(sentenca):
-    """
-    A funcao recebe uma sentenca e devolve uma lista das frases dentro
-    da sentenca.
-    """
+    '''A funcao recebe uma sentenca e devolve uma lista das frases dentro da sentenca'''
     return re.split(r'[,:;]+', sentenca)
 
-
 def separa_palavras(frase):
-    """
-    A funcao recebe uma frase e devolve uma lista das palavras dentro
-    da frase.
-    """
+    '''A funcao recebe uma frase e devolve uma lista das palavras dentro da frase'''
     return frase.split()
 
-
 def n_palavras_unicas(lista_palavras):
-    """
-    Essa funcao recebe uma lista de palavras e devolve o numero de palavras
-    que aparecem uma unica vez.
-    """
+    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras que aparecem uma unica vez'''
     freq = dict()
     unicas = 0
     for palavra in lista_palavras:
@@ -175,12 +56,8 @@ def n_palavras_unicas(lista_palavras):
 
     return unicas
 
-
 def n_palavras_diferentes(lista_palavras):
-    """
-    Essa funcao recebe uma lista de palavras e devolve o numero de palavras
-    diferentes utilizadas.
-    """
+    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras diferentes utilizadas'''
     freq = dict()
     for palavra in lista_palavras:
         p = palavra.lower()
@@ -191,36 +68,73 @@ def n_palavras_diferentes(lista_palavras):
 
     return len(freq)
 
+def compara_assinatura(as_a, as_b):
+    '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve devolver o grau de similaridade nas assinaturas.'''
+    S = 0
+    for i in range (0, 6): #i vai de 0 a 5 = 6 traços linguísticos
+        S = S+ (abs(as_a[i] - as_b[i]))
+    grau = S/6
+    if grau < 0:
+        grau = grau * (-1)
+    return grau
+    pass
 
-def compara_assinatura(ass_main, matriz_ass_input):
-    """
-    Essa funcao recebe duas assinaturas de texto e deve devolver o grau de
-    similaridade nas assinaturas.
-    """
-    lista_Sab = []
-    soma_mod = 0
-    if type(matriz_ass_input[0]) is list:
-        for lin in range(len(matriz_ass_input)):
-            for col in range(len(matriz_ass_input[lin])):
-                soma_mod += abs(ass_main[col] - matriz_ass_input[lin][col])
-            Sab = soma_mod / 6
-            lista_Sab.append(Sab)
-        return lista_Sab
-    else:
-        for i in range(len(matriz_ass_input)):
-            soma_mod += abs(ass_main[i] - matriz_ass_input[i])
-        Sab = soma_mod / 6
-        return Sab
+def calcula_assinatura(texto):
+    '''IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a assinatura do texto.'''
+    lista_de_palavras = separa_palavras(texto)
+    n_total_palavras = len(lista_de_palavras)
 
+    tamanho_palavras = 0
+    for i in range (n_total_palavras):
+        tamanho_palavras = tamanho_palavras + len(lista_de_palavras[i]) #soma do tamanho de todas as palavras
+        
+    tamanho_medio_palavra = tamanho_palavras/n_total_palavras
+    
+    type_token = n_palavras_diferentes(texto)/n_total_palavras
+    
+    hapax_legomana = n_palavras_unicas(texto)/n_total_palavras
 
-def avalia_textos(textos_main, ass_comparadas):
-    """
-    Essa funcao recebe uma lista de textos e deve devolver o numero (0 a n-1)
-    do texto com maior probabilidade de ter sido infectado por COH-PIAH.
-    """
-    aux_ass_com = (ass_comparadas[:])
-    aux_ass_com.sort()
-    for indice in range(len(ass_comparadas)):
-        if aux_ass_com[0] == ass_comparadas[indice]:
-            copiah = indice
-    return copiah -1
+    n_total_sentencas = len(separa_sentencas(texto))
+
+    n_total_frases = len(separa_frases(texto))
+    
+    tamanho_medio_sentenca = tamanho_palavras/n_total_sentencas
+    
+    complexidade_sentenca = n_total_frases/n_total_sentencas
+    
+    tamanho_medio_frase = tamanho_palavras/n_total_frases
+
+    assinatura = [tamanho_medio_palavra, type_token, hapax_legomana, tamanho_medio_sentenca, complexidade_sentenca, tamanho_medio_frase]
+
+    return assinatura
+    
+    pass
+
+def avalia_textos(textos, ass_cp):
+    #IMPLEMENTAR. Essa funcao recebe uma lista de textos e deve devolver o numero (1 a n) do texto com maior probabilidade de ter sido infectado por COH-PIAH.
+    i = 1
+    assinatura_texto = calcula_assinatura(textos[i]) #calcula a assinatura do primeiro texto
+    grau_similaridade = compara_assinatura(assinatura_texto, ass_cp) #verifica o grau de similaridade do primeiro texto com a assinatura do aluno
+                                                                            #infectado com COH-PIAH
+    
+    menor_grau = grau_similaridade  #este será definido como o menor grau de similaridade (quanto mais similares eles forem, menor será o grau)
+    texto_infectado = i   #o texto 0 será definido como o texto infectado, inicialmente
+    i = i+1
+    while i <(len (textos)): #depois e preciso fazer o mesmo com o restante dos textos
+        assinatura_texto = calcula_assinatura(textos[i])
+        grau_similaridade = compara_assinatura(assinatura_texto, ass_cp) #cp = coh-piah
+        if grau_similaridade < menor_grau:  #se o texto sendo analisado tem o grau menor do que o armazenado, 
+            menor_grau = grau_similaridade  #substitui o valor
+            texto_infectado = i             #e também o índice do texto infectado
+        i = i+1
+                  
+    print ("O autor do texto %d está infectado com COH-PIAH" %(texto_infectado))
+    return texto_infectado
+    pass
+
+def main():
+    assinatura_cp = le_assinatura() #lê a assinatura do aluno infectado com COH-PIAH e retorna a assinatura, que é uma lista contendo os 6 traços linguísticos
+    textos_lidos = le_textos()  #lê os textos e retorna uma lista de textos que serão comparados com a assinatura do aluno infectado com COH-PIAH
+    avalia_textos(textos_lidos, assinatura_cp) #todos os textos serão comparados com a assinatura do aluno infectado com COH-PIAH para ver qual é mais parecido
+
+main()
