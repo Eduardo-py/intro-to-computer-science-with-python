56850abca755b0fad9a34a01b1ad8e4b31ce2bc2
ezefranca <ezequiel.ifsp@gmail.com>
2018-02-14 14:03:53 -0200

    ajuste para acentos

--- week_11/ex2.py
+++ week_11/ex2.py
@@ -7,19 +7,26 @@ def menor_nome(names_array):
     
     if len(names_array) > 0:
         sizes = []
+        corrected_names = []
         minimum = sys.maxsize
         index = 0
 
         for name in names_array:
-            raw = unicodedata.normalize("NFKD", name.replace(" ", "")).encode("ASCII","ignore")
-            sizes.append(len(raw))
+            raw = name.replace(" ", "")
+            corrected_names.append(raw)
+
+        for name in corrected_names:
+            raw = unicodedata.normalize("NFKD", name).encode("ASCII","ignore")
+            str_raw = raw.decode("ASCII")
+            print(str_raw)
+            sizes.append(len(str_raw))
 
         for i,value in enumerate(sizes):
             if value < minimum:
                 minimum = value
                 index = i
-        return names_array[index].capitalize()
+        return corrected_names[index].capitalize()
     else:
         return ""
 
-print(menor_nome(['maria', 'josé', 'PAULO', 'Catarina']))
+print(menor_nome(['maria', ' josé ', '   PAULO', 'Catarina   ']))
