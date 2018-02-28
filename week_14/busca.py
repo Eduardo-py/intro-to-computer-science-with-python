4ee86fc369754439a03fdf1224ea002841879e1a
ezefranca <ezequiel.ifsp@gmail.com>
2018-02-28 16:02:46 -0300

    semana 14

---
+++ week_14/busca.py
@@ -0,0 +1,17 @@
+def busca(lista, elemento):
+      
+    first = 0
+    last = len(lista) - 1
+
+    while first <= last:
+        i = (first + last) / 2
+
+        if lista[i] == elemento:
+            return i
+        elif lista[i] > elemento:
+            last = i - 1
+        elif lista[i] < elemento:
+            first = i + 1
+        else:
+            return False
+    return False
